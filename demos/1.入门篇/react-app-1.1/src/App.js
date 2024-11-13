fbc99ec09963049bcf8e11a1628eafb175dd6ee9
zhoutao <mrzater@163.com>
2024-11-13 15:57:29 +0800

    1113

--- demos/1.入门篇/react-app-1.1/src/App.js
+++ demos/1.入门篇/react-app-1.1/src/App.js
@@ -2,33 +2,18 @@
  * @Author: zhoutao mrzater@163.com
  * @Date: 2024-11-12 16:02:54
  * @LastEditors: zhoutao mrzater@163.com
- * @LastEditTime: 2024-11-12 18:09:54
+ * @LastEditTime: 2024-11-13 15:48:16
  * @FilePath: /1.入门篇/react-app-1.1/src/App.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from "react";
 function App() {
-  const id =  'test'
-  const list = [
-    1,2,3,4,5,6
-  ]
-  const element1 = React.createElement('div', {
-    id: 'ele2',
-    style: {
-      color: 'red',
-      fontSize: '25px'
-    }
-  }, 'ele2')
-  const element = list.map((item, index) => <div key={index}>{item}</div>)
+  const handleClick = () => {
+    console.log("hello1");
+  };
   return (
     <>
-      <div id={id} style={{
-        color: '#008c8c',
-        // 注释
-        'fontSize': '20px'
-      }}>Hello</div>
-      {element}
-      {element1}
+      <div onClick={handleClick}>Hello</div>
     </>
   );
 }
---
+++ demos/1.入门篇/react-app-1.1/src/components/Hello.jsx
@@ -0,0 +1,26 @@
+/*
+ * @Author: zhoutao mrzater@163.com
+ * @Date: 2024-11-12 18:35:26
+ * @LastEditors: zhoutao mrzater@163.com
+ * @LastEditTime: 2024-11-12 18:48:34
+ * @FilePath: /1.入门篇/react-app-1.1/src/components/Hello.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react';
+
+function Hello(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+// class Hello extends React.Component{
+//     render () {
+//         return (
+//             <div>hello</div>
+//         )
+//     }
+// }
+export default Hello;
