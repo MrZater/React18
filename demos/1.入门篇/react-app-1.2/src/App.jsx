b3ac6a0819b1ef6d42217a34364643ada6b69619
zhoutao <mrzater@163.com>
2024-11-15 12:04:19 +0800

    1115

--- demos/1.入门篇/react-app-1.2/src/App.jsx
+++ demos/1.入门篇/react-app-1.2/src/App.jsx
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-class App extends Component {
-  state = {
-    value: "",
-  };
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
-  };
-  handleSubmit = (event) => {
-    event.preventDefault();
-    alert(this.state.value);
-  }
-  render() {
-    return (
-      <>
-        <fieldset>
-          <legend>表单</legend>
-          <div>
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-            <button onClick={this.handleSubmit}>提交</button>
-          </div>
-        </fieldset>
-      </>
-    );
-  }
+const App = () => {
+  const [count, setCount] = useState(0);
+  // 每次更新都执行
+  useEffect(() => {
+    console.log('always effect');
+    document.title = `Count: ${count}`
+  }, [count])
+  // 仅在开始执行一次
+  useEffect(() => {
+    console.log('one effect');
+  }, [])
+  // 任何状态值改变，都执行
+  useEffect(() => {
+    console.log('every effect');
+  })
+  return (
+    <div>
+      <h1 onClick={() => setCount(count + 1)}>{count}</h1>
+    </div>
+  );
 }
 
 export default App;
